7f117c07d846f304a76fa2404352d743a9427e7a
rawahulislam <8715096926.abd@gmail.com>
2023-11-01 17:13:45 +0530

    added darkmode btn

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
-import { SocialIcon } from 'react-social-icons'
+import { SocialIcon } from "react-social-icons";
 
 import "../css/homepage.css";
 const Homepage = () => {
-  const [darkmode, setDarkmode] = useState(true)
-  function handleDarkMode(){
-       setDarkmode(!darkmode)
+  const [darkmode, setDarkmode] = useState(true);
+
+  function handleDarkMode() {
+    setDarkmode(!darkmode);
   }
   return (
-    <div className={`homePageContainer ${darkmode?'darkmode' : ""}`}>
-      <div className="headingTxt"><h1>PORTFOLIO</h1></div>
-      <div className="cursorPointer" onClick={handleDarkMode}>Toggle Darkmode</div>
+    <div className={`homePageContainer ${darkmode ? "darkmode" : ""}`}>
+      <div className="headingTxt">
+        <h1>PORTFOLIO</h1>
+      </div>
+      <div className="cursorPointer darkmodeBtn" onClick={handleDarkMode}>
+        Darkmode {darkmode ? "On" : "Off"}
+      </div>
       <div className="boxContainer">
         <div className="leftSection">
           <img
@@ -23,9 +28,21 @@ const Homepage = () => {
           <div className="authorName">Rawahul Islam</div>
           <div>B.Tech in Electrical Engineering</div>
           <div>Full Stack Developer - MERN Stack</div>
-          <div>1 year in Non-Tech and Joined <strong>Pepper Square</strong> as Software Engineer in August 16th 2023 till Present</div>
-          <div><a href="tel:+917006081386">Phone: +917006081386</a></div>
-          <div>WhatsApp <SocialIcon target="blank" href="http://wa.me/+917006081386" url="https://whatsapp.com/" /></div>
+          <div>
+            1 year in Non-Tech and Joined <strong>Pepper Square</strong> as
+            Software Engineer in August 16th 2023 till Present
+          </div>
+          <div>
+            <a href="tel:+917006081386">Phone: +917006081386</a>
+          </div>
+          <div>
+            WhatsApp{" "}
+            <SocialIcon
+              target="blank"
+              href="http://wa.me/+917006081386"
+              url="https://whatsapp.com/"
+            />
+          </div>
           <div className="cursorPointer">Download Resume</div>
         </div>
       </div>
